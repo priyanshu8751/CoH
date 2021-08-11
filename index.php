0628c9dc8234c93a08712b4b771270e260a51b7c
lechurnair <75621929+lechurnair@users.noreply.github.com>
2021-08-11 22:27:13 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
 	<head>
+		    <!-- Required meta tags-->
+		<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title> CoHosp: Covid Information Website </title>
 		<link rel="stylesheet" type="text/css" href="fluid.css"> 
 		<link rel="stylesheet" type="text/css" href="style1.css"> 
