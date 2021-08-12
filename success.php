785a4b7707b2c1d7f1aa60152bca0b21cd263d16
lechurnair <75621929+lechurnair@users.noreply.github.com>
2021-08-12 12:07:25 +0530

    Update success.php

--- success.php
+++ success.php
@@ -1,10 +1,10 @@
 <?php $title="Success";
     include 'includes\header.php';
-   ?>
+   
 
           echo'<h1 class="text-center text-danger"> This page is under construction</h1>';
  
-
+>?
     <a href="index.php" class="btn btn-primary">Go back to Home Page</a>
  
 <?php
